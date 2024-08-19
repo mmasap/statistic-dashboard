559b087c2dc0d0f617ce2d55af43d6486147625e
mmasap <miura.worker@gmail.com>
2024-08-19 13:02:31 +0900

    fix import

---
+++ src/components/charts/LineChart.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import {
+  CategoryScale,
+  type ChartData,
+  Chart as ChartJS,
+  type ChartOptions,
+  Legend,
+  LineElement,
+  LinearScale,
+  PointElement,
+  Title,
+  Tooltip,
+} from 'chart.js'
+import { Line as LineChart } from 'vue-chartjs'
+
+const props = defineProps<{
+  options: ChartOptions<'line'>
+  data: ChartData<'line'>
+}>()
+
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend)
+</script>
+
+<template>
+  <line-chart :options="props.options" :data="props.data" />
+</template>
